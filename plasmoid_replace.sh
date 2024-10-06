7dd49b09185be5bf4845e99ba24eb7806914ab82
Oliver Beard <olib141@outlook.com>
2024-10-06 01:30:16 +0100

    Add script to replace plasmoid locally for testing

---
+++ plasmoid_replace.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Replace installed user plasmoid locally for testing
+
+# The script must always be ran at its location
+cd "$(dirname "$0")" || exit
+
+cp -r package/* ~/.local/share/plasma/plasmoids/org.kde.olib.thermalmonitor
+systemctl restart --user plasma-plasmashell.service
+
+exit 0
